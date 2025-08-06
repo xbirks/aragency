13dd1866edce9c3f0d9acc6b35f80c095584ea62
Andrés Ortega <andres03ortega@gmail.com>
2025-08-06 10:27:20 +0200

    cambio en model card por no poder actualizar los slug de forma automática. Recomiendo borrar lo comentado cuando se pruebe el código

--- components/feed/modelCard.jsx
+++ components/feed/modelCard.jsx
@@ -1,21 +1,48 @@
+// import { motion } from 'framer-motion';
+// import Link from 'next/link';
+
+// export default function ModelCard({ model }) {
+//   return (
+//     <Link href={model.url}>
+//     <motion.div
+//       className="model__tarjeta"
+//       initial={{ opacity: 0, y: 30 }}
+//       animate={{ opacity: 1, y: 0 }}
+//       transition={{ duration: 0.4, ease: 'easeOut' }}
+//     >
+//       <div className="model__tarjeta-img">
+//         <img src={model.images.default} alt={`Foto principal de ${model.name}`} className="model-img-default" />
+//         <img src={model.images.hover} alt={`Foto secundaria de ${model.name}`} className="model-img-hover" />
+//       </div>
+//       <p className="model__tarjeta-name">{model.name}</p>
+//     </motion.div>
+//     </Link>
+//   );
+// }
+
+
+
+
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 export default function ModelCard({ model }) {
+  const href = `/es/modelos/${model.slug}`; // <- SIEMPRE desde el slug
+
   return (
-    <Link href={model.url}>
-    <motion.div
-      className="model__tarjeta"
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.4, ease: 'easeOut' }}
-    >
-      <div className="model__tarjeta-img">
-        <img src={model.images.default} alt={`Foto principal de ${model.name}`} className="model-img-default" />
-        <img src={model.images.hover} alt={`Foto secundaria de ${model.name}`} className="model-img-hover" />
-      </div>
-      <p className="model__tarjeta-name">{model.name}</p>
-    </motion.div>
+    <Link href={href}>
+      <motion.div
+        className="model__tarjeta"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, ease: 'easeOut' }}
+      >
+        <div className="model__tarjeta-img">
+          <img src={model.images.default} alt={`Foto principal de ${model.name}`} className="model-img-default" />
+          <img src={model.images.hover} alt={`Foto secundaria de ${model.name}`} className="model-img-hover" />
+        </div>
+        <p className="model__tarjeta-name">{model.name}</p>
+      </motion.div>
     </Link>
   );
 }
